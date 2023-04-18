29fa85731f8a7e4575d03c14369e730b12abd0ab
louiscklaw <louiscklaw@users.noreply.github.com>
2023-04-18 15:21:22 +0800

    update

---
+++ student.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE Course (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE Student_Course (
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES Student(id),
+    FOREIGN KEY (course_id) REFERENCES Course(id)
+);
+
+INSERT INTO Student (name) VALUES ('Mike');
+INSERT INTO Student (name) VALUES ('Jack');
+INSERT INTO Student (name) VALUES ('Henry');
+
+INSERT INTO Course (name) VALUES ('Math');
+INSERT INTO Course (name) VALUES ('Science');
+INSERT INTO Course (name) VALUES ('History');
+
+INSERT INTO Student_Course (student_id, course_id) VALUES (1, 1);
+INSERT INTO Student_Course (student_id, course_id) VALUES (1, 2);
+INSERT INTO Student_Course (student_id, course_id) VALUES (2, 2);
+INSERT INTO Student_Course (student_id, course_id) VALUES (2, 3);
+INSERT INTO Student_Course (student_id, course_id) VALUES (3, 1);
+INSERT INTO Student_Course (student_id, course_id) VALUES (3, 2);
+INSERT INTO Student_Course (student_id, course_id) VALUES (3, 3);
